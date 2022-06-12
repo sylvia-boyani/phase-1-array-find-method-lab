5cb1cb7d3606d5745585c880cbfed85f84c5fd1c
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-12 08:47:15 +0300

    commit tree b1d3d6b54dcf317776872eea2b5ab967042feb8c

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+const superbowlWin = (records) => {
+    let wins = records.find((record) => record.result === "W");
+ 
+       return wins == undefined ? undefined : wins.year;
+  }; 
